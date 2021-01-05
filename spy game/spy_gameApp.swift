cd8b4f07e24f36b316c8126ea9d985396bf997f5
Elisey <shemetov.elisey@gmail.com>
2021-01-06 00:37:08 +0700

    Initial Commit

---
+++ spy game/spy_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  spy_gameApp.swift
+//  spy game
+//
+//  Created by Елисей on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct spy_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
